33433da1d8f3c0acb088b113cf2578790074b487
raticate <caterina.munoz.v@gmail.com>
2016-02-29 18:57:23 -0300

    11

--- app/views.py
+++ app/views.py
@@ -6,14 +6,14 @@ from app import app
 def index():
 	user = {'nickname': 'Cate'}  # fake user
     posts = [
-    {
-        'author': {'nickname':'John'},
-        'body': {'Beautiful day!!'}
-    },
-    {
-        'author': {'nickname': 'Susan'}, 
-        'body': 'Zootopia was so cool!' }
-        ]
+        {
+            'author': {'nickname':'John'},
+            'body': {'Beautiful day!!'}
+        },
+        {
+            'author': {'nickname': 'Susan'}, 
+            'body': 'Zootopia was so cool!' }
+            ]
 	return render_template('index.html', 
     #title = 'Home', 
     user = user,)
