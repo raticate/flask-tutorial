d1f84e886796534eba053af3f6ffe070b0bcd4da
raticate <caterina.munoz.v@gmail.com>
2016-02-29 19:41:49 -0300

    a

--- app/views.py
+++ app/views.py
@@ -12,4 +12,8 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenId="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', title="Sign In", form=form)
