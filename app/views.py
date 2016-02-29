7bc57fef1280a678df7af9f938e593774f94e80d
raticate <caterina.munoz.v@gmail.com>
2016-02-29 19:31:53 -0300

    a

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 
 @app.route('/')
@@ -7,3 +7,8 @@ def index():
     user = {'nickname': 'Cate'}  # fake user
     posts = [{'author': {'nickname':'John'},'body': 'Beautiful day!!'},{'author': {'nickname': 'Susan'}, 'body': 'Zootopia was so cool!'}]
     return render_template('index.html', user = user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign In", form=form)
